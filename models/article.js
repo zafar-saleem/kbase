68810b97d4df6fe0908113a80b10734df44a11bb
Zafar Saleem <zafarsaleem3@gmail.com>
2015-11-02 19:23:38 +0100

    added post routes on backend

--- models/article.js
+++ models/article.js
@@ -41,3 +41,31 @@ module.exports.getArticleByCategory = function (category, callback) {
 
 	Article.find(query, callback);
 };
+
+module.exports.createArticle = function (newArticle, callback) {
+	newArticle.save(callback);
+};
+
+module.exports.updateArticle = function (id, data, callback) {
+	var title = data.title;
+	var body  = data.body;
+	var category = data.category;
+
+	var query = { _id: id };
+
+	Article.findById(id, function (err, article) {
+		if (!article) {
+			return next(new Error('Could not load article'));
+		} else {
+			article.title = title;
+			article.body = body;
+			article.category = category;
+
+			article.save(callback);
+		}
+	});
+};
+
+module.exports.removeArticle = function (id, callback) {
+	Article.find({ _id: id }).remove(callback);
+};
--- routes/articles.js
+++ routes/articles.js
@@ -33,4 +33,47 @@ router.get('/category/:category', function(req, res, next) {
 	});
 });
 
+router.post('/', function (req, res, next) {
+	var newArticle = new Article({
+		title: req.body.title,
+		category: req.body.category,
+		body: req.body.body
+	});
+
+	Article.createArticle(newArticle, function (err, article) {
+		if (err) console.log(err);
+
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+router.put('/', function (req, res, next) {
+	var id = req.body.id;
+
+	var data = new Article({
+		title: req.body.title,
+		category: req.body.category,
+		body: req.body.body
+	});
+
+	Article.updateArticle(id, data, function (err, article) {
+		if (err) console.log(err);
+
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+router.delete('/:id', function (req, res, next) {
+	var id = req.params.id;
+
+	Article.removeArticle(id, function (err, article) {
+		if (err) console.log(err);
+
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
 module.exports = router;
